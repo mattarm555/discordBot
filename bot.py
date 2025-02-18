307f069ef7a9b65b99e7d44a4ab80278e643dea7
mattarm555 <83235730+mattarm555@users.noreply.github.com>
2025-02-18 16:42:56 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "NzYxNjUyNjM3NzAzNDcxMTA1.GvzNJq.nYT0QRE7bz0-x9mTZiMkiFnRS1OjlUBG4swuHs"
+TOKEN = "NzYxNjUyNjM3NzAzNDcxMTA1.G5RbJE.FzeGuzJvy22vknxhLxdDhQfd2fZPSM3YxdRFWg"
 
 intents = discord.Intents.default()
 intents.message_content = True  # Enable message reading
